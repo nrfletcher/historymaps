cf42c9b20b542a9120e9162b23c5912e01962527
riley <nrileyfletcher@gmail.com>
2023-07-30 00:00:16 -0400

    styles

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <div>
+            <h1><Link href="/About">About</Link></h1>
+        </div>
+    )
+}
---
+++ frontend/pages/About.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/About.module.css';
+
+export default function About() {
+    return (
+        <div className={styles.textbox}>
+            <h1>This is some text</h1>
+        </div>
+    )
+}
