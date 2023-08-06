568e75113577940f0d17b4c7a191149c85f494a9
riley <nrileyfletcher@gmail.com>
2023-08-05 23:59:06 -0400

    dockerfile

--- services/src/test/java/com/historymaps/services/ServicesApplicationTests.java
+++ services/src/test/java/com/historymaps/services/ServicesApplicationTests.java
@@ -1,5 +1,7 @@
 package com.historymaps.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class ServicesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("This is the message", "This is right", "This is correct");
 	}
 
 }
